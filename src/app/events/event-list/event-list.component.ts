251e8cd355c08240238e39e37206173e5466f644
Paco Toro <pacotoro@iMac-de-Paco.local>
2019-06-23 09:22:57 +0200

    Showing event list

--- src/app/events/event-list/event-list.component.ts
+++ src/app/events/event-list/event-list.component.ts
@@ -1,70 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Event } from '../../models/event';
-
-const EVENTS: Event[] = [
-  {
-    id: '0',
-    title: 'Introduction to JS - basic',
-    location: 'Barcelona',
-    date: new Date('2020-03-16'),
-// tslint:disable-next-line: max-line-length
-    description: 'Nulla aliqua duis adipisicing do amet et ullamco commodo id laborum nulla ipsum culpa. Lorem ipsum commodo quis amet consequat nostrud esse est deserunt. Laboris incididunt esse amet sunt tempor pariatur nisi irure nulla veniam id quis elit. Velit officia quis veniam aliqua. Cupidatat velit enim officia dolor ea veniam proident culpa ea duis labore nostrud. Occaecat in velit esse et. Duis anim ad elit ipsum occaecat Lorem veniam labore consequat laboris non.',
-    addedBy: 'user01',
-
-  },
-  {
-    id: '1',
-    title: 'Introduction to Angular',
-    location: 'London',
-    date: new Date('2019-10-28'),
-// tslint:disable-next-line: max-line-length
-    description: 'Nulla aliqua duis adipisicing do amet et ullamco commodo id laborum nulla ipsum culpa. Lorem ipsum commodo quis amet consequat nostrud esse est deserunt. Laboris incididunt esse amet sunt tempor pariatur nisi irure nulla veniam id quis elit. Velit officia quis veniam aliqua. Cupidatat velit enim officia dolor ea veniam proident culpa ea duis labore nostrud. Occaecat in velit esse et. Duis anim ad elit ipsum occaecat Lorem veniam labore consequat laboris non.',
-    addedBy: 'user01',
-  },
-  {
-    id: '2',
-    title: 'Introduction to RXJS',
-    location: 'London',
-    date: new Date('2019-10-02'),
-// tslint:disable-next-line: max-line-length
-    description: 'Nulla aliqua duis adipisicing do amet et ullamco commodo id laborum nulla ipsum culpa. Lorem ipsum commodo quis amet consequat nostrud esse est deserunt. Laboris incididunt esse amet sunt tempor pariatur nisi irure nulla veniam id quis elit. Velit officia quis veniam aliqua. Cupidatat velit enim officia dolor ea veniam proident culpa ea duis labore nostrud. Occaecat in velit esse et. Duis anim ad elit ipsum occaecat Lorem veniam labore consequat laboris non.',
-    addedBy: 'user01',
-  },
-  {
-    id: '3',
-    title: 'AWS',
-    location: 'Berlin',
-    date: new Date('2019-11-21'),
-// tslint:disable-next-line: max-line-length
-    description: 'Nulla aliqua duis adipisicing do amet et ullamco commodo id laborum nulla ipsum culpa. Lorem ipsum commodo quis amet consequat nostrud esse est deserunt. Laboris incididunt esse amet sunt tempor pariatur nisi irure nulla veniam id quis elit. Velit officia quis veniam aliqua. Cupidatat velit enim officia dolor ea veniam proident culpa ea duis labore nostrud. Occaecat in velit esse et. Duis anim ad elit ipsum occaecat Lorem veniam labore consequat laboris non.',
-    addedBy: 'user01',
-  },
-  {
-    id: '4',
-    title: 'Angular NgRx - introduction',
-    location: 'Madrid',
-    date: new Date('2019-12-05'),
-// tslint:disable-next-line: max-line-length
-    description: 'Nulla aliqua duis adipisicing do amet et ullamco commodo id laborum nulla ipsum culpa. Lorem ipsum commodo quis amet consequat nostrud esse est deserunt. Laboris incididunt esse amet sunt tempor pariatur nisi irure nulla veniam id quis elit. Velit officia quis veniam aliqua. Cupidatat velit enim officia dolor ea veniam proident culpa ea duis labore nostrud. Occaecat in velit esse et. Duis anim ad elit ipsum occaecat Lorem veniam labore consequat laboris non.',
-    addedBy: 'user01',
-  }
-];
-
+import { EventService } from '../../core/event.service';
 
 @Component({
   selector: 'eui-event-list',
   templateUrl: './event-list.component.html',
   styleUrls: ['./event-list.component.scss']
 })
-export class EventListComponent implements OnInit {
+export class EventListComponent implements OnInit, OnDestroy {
+  events: Event[];
+  selectedEvent: Event;
 
-  events = EVENTS;
 
-  selectedEvent = EVENTS[0];
-
-  constructor() { }
+  constructor(
+    private eventService: EventService,
+  ) {}
 
   ngOnInit() {
+    this.getEvents();
+  }
+
+  getEvents() {
+    this.eventService.getEvents().subscribe((events: Event[]) => {
+      this.events = events;
+      this.selectedEvent = events[0];
+    });
+  }
+
+  ngOnDestroy() {
   }
 
 }
