2449a5110d37ae8f571f322cf843c17c51c69251
Paco Toro <pacotoro@iMac-de-Paco.local>
2019-09-08 16:37:03 +0200

    Updating error messages

--- src/app/core/global-error-handler.service.ts
+++ src/app/core/global-error-handler.service.ts
@@ -21,11 +21,18 @@ export class GlobalErrorHandlerService implements ErrorHandler {
         if (!navigator.onLine) {
           // Handle offline error
           this.notificationService.showError('Communication error', 'No Internet Connection');
+        } else if (error.status >= 400 && error.status < 500) {
+          this.notificationService.showError('Error doing a request to the server',
+                                              'The request contains bad syntax or cannot be fulfilled.');
+        } else if (error.status >= 500) {
+          this.notificationService.showError('Error in the response of the server',
+                                              'The server failed to fulfill an apparently valid request');
         } else {
-          this.notificationService.showError(`${error.status}`, error.message);
+          this.notificationService.showError(error.statusText,
+                                              'An error has occurred trying to communicate with server.');
         }
       } else {
-        this.notificationService.showError('Unexpected error', error.message);
+        this.notificationService.showError(error.name, error.message);
       }
     });
   }
